ec9064d88f3817cbc48febdd83137aadbdc38694
kirill3518 <kirill3518@mail.ru>
2022-03-30 17:31:20 +0500

    ДЗ № 2.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import Message from './Message';
 
 const myName = 'Kirill';
 ReactDOM.render(
   <React.StrictMode>
     <App name={myName} />
+    <Message name={myName} />
   </React.StrictMode>,
   document.getElementById("root")
 );
